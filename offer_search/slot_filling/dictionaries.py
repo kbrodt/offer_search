541b3f62b9d2be3cf6c1989c37829e0c9e469e9a
nrshka <retributioniswar@gmail.com>
2019-01-28 17:01:31 +0700

    SlotFller with metaclass

--- offer_search/slot_filling/dictionaries.py
+++ offer_search/slot_filling/dictionaries.py
@@ -1,29 +1,28 @@
 '''import abc
 
 __all__ = [
-	"Dictionary"
+    "Dictionary"
 ]
 
 class Dictionary(metaclass=abc.ABCMeta):
-	@abc.abstractmethod
-	def __getitem__(self, key):
-		pass
-	@abc.abstractmethod
-	def ParseFile(file : str) -> int:
-		pass
-	
+    @abc.abstractmethod
+    def __getitem__(self, key):
+        pass
+    @abc.abstractmethod
+    def ParseFile(file : str) -> int:
+        pass
+    
 '''
-class SportGoods(object):
-	def __init__(self):
-		self.goods = [
-			"qwe"
-		]
-	def __getitem__(self, key):
-		return self.goods[int(key)]
-
-goodsClasses = [
-	SportGoods
-]
-
-def getGoodsDictionary(type):
-	return goodsClasses[type]()
+class Goods(object):
+    def __init__(self, intent):
+        self.goods = [
+            "велик"
+        ]
+    def __getitem__(self, key):
+        return self.goods[int(key)]
+    #@overrides
+    def Parse(self, file, bracket):
+        #bracket - символ, отделяющий название от описания
+        with open(file, "r") as file:
+            for line in file:
+                self.goods.append((line)[0])
