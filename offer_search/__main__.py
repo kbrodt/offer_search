77152dce8c1db16bd6b7fb12140888f36be62f66
ameyuuno <ameyuuno@gmail.com>
2019-01-29 00:46:12 +0700

    replaced intent classifier mock with implementation based on logistic regression

--- offer_search/__main__.py
+++ offer_search/__main__.py
@@ -11,39 +11,51 @@
 
 import json
 import typing as t
+from pathlib import Path
 
 from overrides import overrides
 
 from offer_search.intent_classification import IntentClassifier
+from offer_search.intent_classification import LogRegIntentClassifier
+from offer_search.searcher import Searcher
 from offer_search.slot_filling import SlotFiller
 from offer_search.ranking import Ranker
-from offer_search.searcher import Searcher
 
 
-class IntentClassifierMock(IntentClassifier):
-    @overrides
-    def predict(self, text: str) -> str:
-        return ''
+def create_intent_classifier() -> IntentClassifier:
+    resource_directory = Path('./resources/intent_classification')
+
+    return LogRegIntentClassifier(
+        resource_directory / 'vectorizer.joblib',
+        resource_directory / 'classifier.joblib',
+        resource_directory / 'label_encoder.joblib',
+    )
 
 
-class SlotFillerMock(SlotFiller):
-    @overrides
-    def fill(self, text: str, intent: str) -> t.Dict[str, t.Any]:
-        return dict()
+def create_slot_filler() -> SlotFiller:
+    class SlotFillerMock(SlotFiller):
+        @overrides
+        def fill(self, text: str, intent: str) -> t.Dict[str, t.Any]:
+            return dict()
+
+    return SlotFillerMock()
 
 
-class RankerMock(Ranker):
-    @overrides
-    def rank(self, search_form: t.Dict[str, t.Any]) -> t.List[t.Dict[str, t.Any]]:
-        return list()
+def create_ranker() -> Ranker:
+    class RankerMock(Ranker):
+        @overrides
+        def rank(self, search_form: t.Dict[str, t.Any]) -> t.List[t.Dict[str, t.Any]]:
+            return list()
+
+    return RankerMock()
 
 
 def main() -> t.NoReturn:
     """Entrypoint
     """
 
-    searcher = Searcher(IntentClassifierMock(), SlotFillerMock(), RankerMock())
-    
+    searcher = Searcher(create_intent_classifier(), create_slot_filler(), create_ranker())
+
     while True:
         search_request = input("search => ")
 
