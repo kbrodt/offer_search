8214dc5d0aff8d83615bac7a3905f85ddbad1b82
ameyuuno <ameyuuno@gmail.com>
2019-01-28 22:09:18 +0700

    added normalizer based on pymorphy2

---
+++ offer_search/utils/processing/text/stages/normalizers.py
@@ -0,0 +1,35 @@
+#
+#  Project: Offer Search
+#
+#  Course: CompTech2019
+#      Novosibirsk State University
+#
+#  Created by ameyuuno on 2019-01-28
+#
+#  GitHub: @ameyuuno
+#
+
+import itertools as it
+import typing as t
+
+from pymorphy2 import MorphAnalyzer
+from overrides import overrides
+
+from offer_search.utils.processing.text.text_processing_stages import Normalizer
+
+
+__all__ = [
+    'Pymorphy2Normalizer',
+]
+
+
+class Pymorphy2Normalizer(Normalizer):
+    def __init__(self) -> None:
+        self.__morph_analyzer = MorphAnalyzer
+
+    @overrides
+    def normalize(self, tokens: t.List[str]) -> t.List[str]:
+        return [self.__normalize(token) for token in tokens]
+
+    def __normalize(self, word: str) -> str:
+        return self.__morph_analyzer.parse(word)[0].normal_form
