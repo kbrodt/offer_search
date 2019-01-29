8a2b7ff802c93eab3a4dd499a8db7ac8dea9ad9f
ameyuuno <ameyuuno@gmail.com>
2019-01-30 00:08:38 +0700

    updated elasticsearch ranker

--- offer_search/ranking/elasticsearch/ranker.py
+++ offer_search/ranking/elasticsearch/ranker.py
@@ -11,6 +11,7 @@
 
 import typing as t
 
+from elasticsearch import Elasticsearch
 from overrides import overrides
 
 from offer_search.ranking.base import Ranker
@@ -22,6 +23,40 @@ __all__ = [
 
 
 class ElasticsearchRanker(Ranker):
+    __DEFAULT_INDEX = 'offer_search'
+    __DEFAULT_DOC_TYPE = 'offer'
+
+    def __init__(
+        self, 
+        es_host: str = 'localhost', 
+        es_port: int = 9200,
+        index: str = __DEFAULT_INDEX,
+        doc_type: str = __DEFAULT_DOC_TYPE,
+        preset: t.Optional[t.List[t.Dict[str, t.Any]]] = None,
+    ) -> None:
+        self.__elasticsearch = Elasticsearch([{
+            'host': es_host,
+            'port': es_port,
+        }])
+
+        if not self.__elasticsearch.ping():
+            raise ValueError(f"Can not connect to Elasticsearch: {es_host}:{es_port}")
+
+        self.__index = index
+        self.__doc_type = doc_type
+
+        if preset is not None:
+            self.__preset(preset)
+
     @overrides
     def rank(self, search_form: t.Dict[str, t.Any]) -> t.List[t.Dict[str, t.Any]]:
-        pass
+        raise NotImplementedError
+
+    def __preset(self, preset: t.List[t.Dict[str, t.Any]]) -> None:
+        for record in preset:
+            self.__elasticsearch.index(
+                index=self.__index, 
+                doc_type=self.__doc_type, 
+                body=record,
+            )
+
