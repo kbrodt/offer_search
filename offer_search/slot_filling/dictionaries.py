f2012d2d37c483461735e666a9ab84165b20a8c9
nrshka <retributioniswar@gmail.com>
2019-01-30 19:53:45 +0700

    dictionaries.py parses slots in data folder

--- offer_search/slot_filling/dictionaries.py
+++ offer_search/slot_filling/dictionaries.py
@@ -1,29 +1,30 @@
-'''import abc
+import pymorphy2 as pmh
 
-__all__ = [
-    "Dictionary"
-]
 
-class Dictionary(metaclass=abc.ABCMeta):
-    @abc.abstractmethod
-    def __getitem__(self, key):
-        pass
-    @abc.abstractmethod
-    def ParseFile(file : str) -> int:
-        pass
-    
-'''
 class Goods(object):
-    def __init__(self, intent):
+    def __init__(self, intent: int):
+        self.analyzer = pmh.MorphAnalyzer()
         self.goods = [
             "велик",
             "велосипед"
         ]
+        self.paths = [
+            "..//..//data//sport//slots.csv",
+            "..//..//data//food//slots.csv"
+        ]
+        self.parse(self.paths[int(intent)], ' ')
     def __getitem__(self, key):
         return self.goods[int(key)]
     #@overrides
-    def Parse(self, file, bracket):
+    def parse(self, file : str, bracket : str):
         #bracket - символ, отделяющий название от описания
-        with open(file, "r") as file:
+        with open(file, "r", encoding='utf-8') as file:
             for line in file:
-                self.goods.append((line)[0])
+                line = line.replace('\n', '')
+                words = line.split(bracket)
+                for word in words:
+                    tokens = self.analyzer.parse(word)
+                    for token in tokens:
+                        self.goods.append(token.normal_form)
+        #исключаем повторы
+        self.goods = list(set(self.goods))
