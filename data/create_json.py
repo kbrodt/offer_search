81ed002bcf2e1109a50fbae6dd9a879403f99bcd
Kirill Brodt <kirill.brodt@yandex.ru>
2019-02-01 15:45:54 +0700

    function to create preset.json is added

--- data/create_json.py
+++ data/create_json.py
@@ -7,6 +7,7 @@ path_sport = './sport'
 all_files_food = os.listdir(path_food)
 all_files_sport = os.listdir(path_sport)
 
+
 def create_json(path):
     data = pd.read_excel(os.path.join(path, 'data_cleaned.xls'))
     data_frame = pd.DataFrame(data)
@@ -18,6 +19,7 @@ def create_json(path):
                 'Item': str(row[0]).lower(),
                 'Attributes': '' if row[1] != row[1] else row[1],
                 'Price': int(row[2]),
+                
                 'Offer': row_meta[0],
                 'Web': row_meta[1],
                 'Cashback': 0 if row_meta[2] != row_meta[2] else row_meta[2],
@@ -26,7 +28,8 @@ def create_json(path):
                 'Advert_text': '' if row_meta[5] != row_meta[5] else row_meta[5]
             }
             all_json.append(new_json)
-			
+
+
 all_json = []
 for directory in all_files_food:
     if os.path.isdir(os.path.join(path_food, directory)):
@@ -34,8 +37,7 @@ for directory in all_files_food:
 for directory in all_files_sport:
     if os.path.isdir(os.path.join(path_sport, directory)):
         create_json(os.path.join(path_sport, directory))
-		
-#len(all_json)
 
-with open('./preset.json','w') as fout:
+
+with open('../resources/ranking/preset.json', 'w', encoding='utf-8') as fout:
     json.dump(all_json, fout)
