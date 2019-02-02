8e0aefc46a851682dee4a089205158c7d2eb6800
ameyuuno <ameyuuno@gmail.com>
2019-02-03 02:13:04 +0700

    created run script

---
+++ run.py
@@ -0,0 +1,16 @@
+#
+#  Project: Offer Search
+#
+#  Course: CompTech2019
+#      Novosibirsk State University
+#
+#  Created by ameyuuno on 2019-02-02
+#
+#  GitHub: @ameyuuno
+#
+
+from offer_search import start_service
+
+
+if __name__ == '__main__':
+    start_service()
