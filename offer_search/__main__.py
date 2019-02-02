5e0e2e566ce37271e6d1933214b39bec01cf23ff
ameyuuno <ameyuuno@gmail.com>
2019-02-03 02:31:26 +0700

    moved creating components to core __init__

--- offer_search/__main__.py
+++ offer_search/__main__.py
@@ -15,48 +15,10 @@ from pathlib import Path
 
 from overrides import overrides
 
-from offer_search.core.intent_classification import IntentClassifier
-from offer_search.core.intent_classification.standard import CompositeVectorizer
-from offer_search.core.intent_classification.standard import LogisticRegressionModel
-from offer_search.core.intent_classification.standard import Preprocessor
-from offer_search.core.intent_classification.standard import StandardIntentClassifier
-from offer_search.core.intent_classification.standard import TfidfVectorizer
+from offer_search.core import create_intent_classifier
+from offer_search.core import create_ranker
+from offer_search.core import create_slot_filler
 from offer_search.core.searcher import Searcher
-from offer_search.core.slot_filling import SlotFiller
-from offer_search.core.slot_filling.slot_filling import SlotFillerWithRules
-from offer_search.core.ranking import Ranker
-from offer_search.core.ranking.elasticsearch import ElasticsearchRanker
-
-
-def create_intent_classifier() -> IntentClassifier:
-    resource_directory = Path('./resources/intent_classification')
-
-    return StandardIntentClassifier(
-        Preprocessor(download_if_missing=True),
-        CompositeVectorizer([
-            TfidfVectorizer(resource_directory / 'over_words_vectorizer.joblib'),
-            TfidfVectorizer(resource_directory / 'over_trigrams_vectorizer.joblib'),
-        ]),
-        LogisticRegressionModel(
-            resource_directory / 'classifier.joblib',
-            resource_directory / 'label_encoder.joblib',
-        ),
-    )
-
-
-def create_slot_filler() -> SlotFiller:
-    return SlotFillerWithRules()
-
-
-def create_ranker() -> Ranker:
-    with Path('./resources/ranking/preset_small.json').open('r') as preset_file:
-        preset: t.List[t.Dict[str, t.Any]] = json.load(preset_file)
-
-    return ElasticsearchRanker(
-        es_host='localhost',
-        es_port=9200,
-        preset=preset,
-    )
 
 
 def main() -> t.NoReturn:
