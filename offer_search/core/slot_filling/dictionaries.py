65c178b26d532ebc0d80787f1c4fb851215ada0f
ameyuuno <ameyuuno@gmail.com>
2019-02-03 11:49:19 +0700

    fixed dictionaries

--- offer_search/core/slot_filling/dictionaries.py
+++ offer_search/core/slot_filling/dictionaries.py
@@ -18,14 +18,9 @@ class Goods(object):
     def __getitem__(self, key):
         return self.goods[int(key)]
     #@overrides
-<<<<<<< HEAD:offer_search/slot_filling/dictionaries.py
-    def parse(self, file : str, bracket : str):
-        with open(file, "r", encoding='utf-8') as file:
-=======
     def parse(self, file: Path, bracket: str):
         #bracket - символ, отделяющий название от описания
         with file.open("r", encoding='utf-8') as file:
->>>>>>> develop:offer_search/core/slot_filling/dictionaries.py
             parser = Parser(ATTRIBUTE)
             for line in file:
                 line = line.replace('\n', '')
